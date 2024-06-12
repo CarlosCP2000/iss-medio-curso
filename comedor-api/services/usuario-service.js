4d8728a45e196e8b67f6332e6754a95f1fbcd0fd
Ader Trinidad Evaristo <simonsantiagoalex@gmail.com>
2024-06-11 19:53:14 -0500

    Actualizacion en los archivos deployment

--- comedor-api/services/usuario-service.js
+++ comedor-api/services/usuario-service.js
@@ -3,7 +3,7 @@ module.exports={
 
     post: async function(username) {
         try {
-            const response = await axios.post("http://localhost:8081/usuario", { username });
+            const response = await axios.post("172.31.83.110:8081/usuario", { username });
             return response.data;
         } catch (error) {
             console.error("Error en la solicitud POST:", error);
